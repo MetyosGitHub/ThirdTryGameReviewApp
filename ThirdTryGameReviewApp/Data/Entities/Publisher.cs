c6d87b70202159bb0fb6471b5972188f8b492e7b
Dimitar Gramenov <goshoburkana123@gmail.com>
2022-12-04 18:50:25 +0200

    Added Entity Publisher

---
+++ ThirdTryGameReviewApp/Data/Entities/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThirdTryGameReviewApp.Data.Entities
+{
+    public class Publisher
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
