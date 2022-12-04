9db438f6e5acb06e8a0b3b1f6e8481aa0113bd9e
Dimitar Gramenov <goshoburkana123@gmail.com>
2022-12-04 18:48:34 +0200

    Added Entity Genre

---
+++ ThirdTryGameReviewApp/Data/Entities/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThirdTryGameReviewApp.Data.Entities
+{
+    public class Genre
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
