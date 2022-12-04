070c21dbb2686b09015419dd531c1bd3d04bac37
Dimitar Gramenov <goshoburkana123@gmail.com>
2022-12-04 18:59:16 +0200

    Added Entity Review

---
+++ ThirdTryGameReviewApp/Data/Entities/Review.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThirdTryGameReviewApp.Data.Entities
+{
+    public class Review
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Score { get; set; }
+        [Required]
+        public int GameId { get; set; }
+        [Required]
+        [ForeignKey(nameof(GameId)]
+        public Game Game { get; set; }
+    }
+}
