f62235f13003e5ff58f6a20bedf1b645056dbab6
Dimitar Gramenov <goshoburkana123@gmail.com>
2022-12-04 18:44:09 +0200

    AddedEntities-Game and User

---
+++ ThirdTryGameReviewApp/Data/Entities/Game.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThirdTryGameReviewApp.Data.Entities
+{
+    public class Game
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int YearOfCreation { get; set; }
+
+        public List<Review> GameReviews { get; set; } = new List<Review>();
+
+        public string? PictureURL { get; set; }
+
+        public int GenreId { get; set; }
+        [ForeignKey(nameof(GenreId))]
+        public Genre Genre { get; set; }
+
+        public int PublisherId { get; set; }
+        [ForeignKey(nameof(PublisherId))]
+        public Publisher Publisher { get; set; }
+    }
+}
---
+++ ThirdTryGameReviewApp/Data/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThirdTryGameReviewApp.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(25), MinLength(3)]
+        public string Name { get; set; }
+
+
+    }
+}
